4b6df58f2520986d828f5d9896b6ec5275b3bf59
arqdsis <arqdsis@mocli10209.animaedu.intranet>
2018-05-08 19:13:06 -0300

    Aula07 parte 02

---
+++ Exercices_PatternsAula07_parte_02-master/Exercice_Pattern/Exercicio1/Incremental.java
@@ -0,0 +1,43 @@
+package Exercicio1;
+/**
+ * 
+ * @author Lincoln de freitas almeida 816119071  SI3ANMCA
+ * 			
+ *
+ */
+public class Incremental {
+
+	private static final Object lock = new Object();
+
+	private static Incremental singletonInstance;
+
+	private static int count = 0;
+
+	private int numero;
+
+	private Incremental() {
+		numero = ++count;
+	}
+
+	public static Incremental getInstance() {
+		synchronized (lock) {
+			if (singletonInstance == null)
+				singletonInstance = new Incremental();
+		}
+
+		return singletonInstance;
+	}
+
+	public String toString() {
+		return "Incremental " + numero;
+	}
+
+	public static class TesteIncremental {
+		public static void main(String[] args) {
+			for (int i = 0; i < 10; i++) {
+				Incremental inc = new Incremental();
+				System.out.println(inc);
+			}
+		}
+	}
+}
---
+++ Exercices_PatternsAula07_parte_02-master/Exercice_Pattern/Exercicio2/SingletonExercise.java
@@ -0,0 +1,15 @@
+package Exercicio2;
+/**
+ * 
+ * @author Lincoln de freitas almeida 816119071  SI3ANMCA
+ * 			
+ *
+ */
+public class SingletonExercise {
+	public static void main(String args[]) {
+		Deck deck = new Deck( );
+		
+		deck.print( );
+		}
+
+}
